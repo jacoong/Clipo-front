e0a3c6fce5de8cc6f47f6338a1a57c7667754b07
jacoong <87224594+jacoong@users.noreply.github.com>
2024-08-05 17:43:12 +0900

    sefsefs

--- src/store/ApiService.ts
+++ src/store/ApiService.ts
@@ -1,53 +1,89 @@
-import { instance,formInstance } from './axios_context';
-import { LoginType, SMS, SMSValidate,socialLogin,usernameProfile } from './types';
+import { instance, formInstance } from './axios_context';
+import { LoginType, SMS, SMSValidate, socialLogin, usernameProfile } from './types';
 
 class AuthService {
   static async login(userData: LoginType): Promise<any> {
-    const response = await instance.post('api/auth/login', userData);
+    const response = await instance.post('api/auth/login', userData, {
+      headers: {
+        'Content-Type':  'application/json',
+      },
+    });
     return response.data;
   }
 
   static async signUp(userData: LoginType): Promise<any> {
-    const response = await instance.post('api/auth/signup', userData);
+    const response = await instance.post('api/auth/signup', userData, {
+      headers: {
+        'Content-Type':  'application/json',
+      },
+    });
     return response.data;
   }
 
   static async smsRequest(userData: SMS): Promise<any> {
-    const response = await instance.post('api/auth/send/phone', userData);
+    const response = await instance.post('api/auth/send/phone', userData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     return response.data;
   }
 
   static async forgetPassword(userData: any): Promise<any> {
-    const response = await instance.post(`api/auth/recreatePassword/${userData}`);
+    const response = await instance.post(`api/auth/recreatePassword/${userData}`, null, {
+      headers: {
+        'Content-Type':  'application/json',
+      },
+    });
     return response.data;
   }
 
   static async smsVerificate(userData: SMSValidate): Promise<any> {
-    const response = await instance.post('api/auth/send/verification', userData);
+    const response = await instance.post('api/auth/send/verification', userData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     return response.data;
   }
 
   static async updatePassword(userData: { newPassword: string; oldPassword: string }): Promise<any> {
-    const response = await instance.post('api/update/password', userData);
+    const response = await instance.post('api/update/password', userData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     return response.data;
   }
 
-  static async socialLogin(userData:socialLogin): Promise<any> {
-    const response = await instance.post('api/auth/socialLogin', userData);
+  static async socialLogin(userData: socialLogin): Promise<any> {
+    const response = await instance.post('api/auth/socialLogin', userData, {
+      headers: {
+        'Content-Type':  'application/json',
+      },
+    });
     return response.data;
   }
 }
 
 class UserService {
   static async getUserProfile(): Promise<any> {
-    const response = await instance.get(`/api/get/user/information`);
+    const response = await instance.get(`/api/get/user/information`, {
+      headers: {
+        'Content-Type':'application/json',
+      },
+    });
     return response.data;
   }
 
-  static async createNicknameProfileImg(userData:FormData): Promise<any> {
-    const response = await formInstance.post('/api/update/profileNickname', userData);
+  static async createNicknameProfileImg(userData: FormData): Promise<any> {
+    const response = await instance.post('/api/update/profileNickname', userData, {
+      headers: {
+        'Content-Type': 'multipart/form-data' 
+      },
+    });
     return response.data;
   }
 }
 
-export default {AuthService,UserService};
+export default { AuthService, UserService };
--- src/store/axios_context.tsx
+++ src/store/axios_context.tsx
@@ -21,9 +21,6 @@ export const refreshAxios = axios.create({
   export  const instance = axios.create({
     baseURL: `${process.env.REACT_APP_SERVER_URL}`,
     withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-      }
   });
 
   export  const formInstance = axios.create({
